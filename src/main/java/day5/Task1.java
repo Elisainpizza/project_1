06d3c2d7782264478abf0661b3efcfe54e573fa5
Elisainpizza <axolotl2995@gmail.com>
2021-08-28 21:33:39 +0300

    complete Day 5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,31 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car auto = new Car();
+        auto.setModel("Wolksvagen");
+        auto.setColor("Blue");
+        auto.setYearOfIssue(2021);
+        auto.speak();
+    }
+}
 
+class Car {
+    private String model;
+    private String color;
+    private int yearOfIssue;
+
+    public void setModel(String model) {this.model = model;}
+    public String getModel() {return model;}
+
+    public void setColor(String color) {this.color = color;}
+    public String getColor() {return color;}
+
+    public void setYearOfIssue(int yearOfIssue) {this.yearOfIssue = yearOfIssue;}
+    public int getYearOfIssue() {return yearOfIssue;}
+
+    public void speak(){
+        System.out.println("Model: " + model);
+        System.out.println("Color: " + color);
+        System.out.println("Year of issue: " + yearOfIssue);
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,44 @@
 package day5;
 
-
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike first = new Motorbike("Volvo", "Blue", 2021);
+        first.speak();
+    }
+}
 
+class Motorbike {
+    private String model;
+    private String color;
+    private int yearOfIssue;
+
+    public Motorbike(String model, String color, int yearOfIssue){
+        if (model.isEmpty()) {
+            System.out.println("Enter model of motorbike");
+        }
+        else { this.model = model; }
+
+        if (color.isEmpty()) {
+            System.out.println("Enter color of motorbike");
+        }
+        else { this.color = color; }
+
+        this.yearOfIssue = yearOfIssue;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public String getColor() {
+        return color;
+    }
+    public int getYearOfIssue() {
+        return yearOfIssue;
+    }
+
+    public void speak() {
+        System.out.println("Model: " + model);
+        System.out.println("Color: " + color);
+        System.out.println("Year of issue: " + yearOfIssue);
     }
 }
