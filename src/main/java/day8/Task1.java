d4f1f587c0cac159ffcee1af08ef32753a74bf09
Elisainpizza <axolotl2995@gmail.com>
2021-09-08 23:24:18 +0300

    complete Day 8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,26 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        long i = 0;
+        while (i < 20000) {
+            System.out.print(i + " ");
+            i++;
+        }
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime - startTime));
+
+
+
+        long startTime2 = System.currentTimeMillis();
+        StringBuilder builder = new StringBuilder(0);
+        int count = 20000;
+        for (int j = 0; j < count; j++) {
+           builder.append(j + " ");
+        }
+        System.out.println(builder.toString());
+        long stopTime2 = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime2 - startTime2));
 
     }
 }
